79b6259a5df3cf9ac01242bc59a2a37f1c243f09
Edoardo Zappia <ezappia@login01.hpc.rd.areasciencepark.it>
2024-04-12 14:46:28 +0000

    m

--- HPC_ex2/mpi_weak_epyc.sh
+++ HPC_ex2/mpi_weak_epyc.sh
@@ -16,7 +16,7 @@ echo "MPI_Processes,Columns,Rows,Execution_Time" > $OUTPUT_CSV
 BASE_COLS=800
 BASE_ROWS=600
 
-for total_procs in {2..512..2}; do
+for total_procs in {2..256..2}; do
     let cols=BASE_COLS*total_procs
     let rows=BASE_ROWS*total_procs
     mpirun -np $total_procs ./mandelbrot $cols $rows -2.0 -1.0 1.0 1.0 255
