a620cc1e5b5e5cae06d14e468d45b540f7380b40
Edoardo Zappia <ezappia@login01.hpc.rd.areasciencepark.it>
2024-05-01 13:52:09 +0000

    m

--- HPC_ex2/mpi_weak_epyc.sh
+++ HPC_ex2/mpi_weak_epyc.sh
@@ -13,12 +13,12 @@ export OMP_NUM_THREADS=1
 OUTPUT_CSV="mpi_weak_epyc.csv"
 echo "MPI_Processes,Columns,Rows,Execution_Time" > $OUTPUT_CSV
 
-BASE_COLS=600
+BASE_COLS=800
 BASE_ROWS=600
 
-for total_procs in 2 4 8 16 32 48 64 96 128 160 192 224 256; do
-    cols=$(echo "sqrt($BASE_COLS*$total_procs)" | bc)
-    rows=$(echo "sqrt($BASE_ROWS*$total_procs)" | bc)
+for total_procs in {2..96..2}; do
+    let cols=BASE_COLS*total_procs
+    let rows=BASE_ROWS*total_procs
     mpirun -np $total_procs ./mandelbrot $cols $rows -2.0 -1.0 1.0 1.0 255
     # Leggi il tempo di esecuzione dal file temporaneo
     execution_time=$(<temp_execution_time.txt)
