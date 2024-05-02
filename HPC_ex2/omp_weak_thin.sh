dc996ac4d70a3f9d13794d0d7559b4b91b04b812
EdoardoZappia <edoardozappia5@gmail.com>
2024-05-02 16:50:20 +0200

    m

--- HPC_ex2/omp_weak_thin.sh
+++ HPC_ex2/omp_weak_thin.sh
@@ -30,7 +30,7 @@ for OMP_NUM_THREADS in {1..24}; do
     let cols=$BASE_COLS
     let rows=$((BASE_ROWS*OMP_NUM_THREADS))
     # Esegui il programma e salva il tempo di esecuzione
-    execution_time=$(mpirun -np 1 --map-by socket --bind-to socket ./mandelbrot $cols $rows -2.0 -1.0 1.0 1.0 255 $OMP_NUM_THREADS)
+    execution_time=$(mpirun -np 1 --map-by socket --bind-to socket ./mandel $cols $rows -2.0 -1.0 1.0 1.0 255 $OMP_NUM_THREADS)
 
     echo "$OMP_NUM_THREADS,$cols,$rows,$execution_time" >> $OUTPUT_CSV
 
